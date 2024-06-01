f58ca244202a13d245b8d41cb8f928df9862e227
hendisantika <hendisantika@yahoo.co.id>
2024-06-01 18:50:31 +0700

    Add consumeMessages in Consumer 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootawssqssample/config/Consumer.java
+++ src/main/java/id/my/hendisantika/springbootawssqssample/config/Consumer.java
@@ -1,9 +1,11 @@
 package id.my.hendisantika.springbootawssqssample.config;
 
 import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,6 +24,25 @@ import org.springframework.stereotype.Service;
 public class Consumer {
 
     private final AmazonSQS amazonSQSClient;
+
     @Value("${aws.queueName}")
     private String queueName;
+
+    @Scheduled(fixedDelay = 5000) // It runs every 5 seconds.
+    public void consumeMessages() {
+        try {
+            String queueUrl = amazonSQSClient.getQueueUrl(queueName).getQueueUrl();
+
+            ReceiveMessageResult receiveMessageResult = amazonSQSClient.receiveMessage(queueUrl);
+
+            if (!receiveMessageResult.getMessages().isEmpty()) {
+                com.amazonaws.services.sqs.model.Message message = receiveMessageResult.getMessages().get(0);
+                log.info("Read Message from queue: {}", message.getBody());
+                amazonSQSClient.deleteMessage(queueUrl, message.getReceiptHandle());
+            }
+
+        } catch (Exception e) {
+            log.error("Queue Exception Message: {}", e.getMessage());
+        }
+    }
 }
