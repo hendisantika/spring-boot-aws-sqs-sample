0f6dedf7a92be74e8ac73f8dffbd1df5f744f446
hendisantika <hendisantika@yahoo.co.id>
2024-06-01 18:47:02 +0700

    Add SqsConfig 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootawssqssample/config/SqsConfig.java
@@ -0,0 +1,39 @@
+package id.my.hendisantika.springbootawssqssample.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aws-sqs-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/06/24
+ * Time: 18.45
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SqsConfig {
+
+    @Value("${aws.access.key}")
+    private String accessKey;
+
+    @Value("${aws.secret.key}")
+    private String secretKey;
+
+    @Bean
+    public AmazonSQS amazonSQSClient() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSQSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.AP_SOUTHEAST_1)
+                .build();
+    }
+}
