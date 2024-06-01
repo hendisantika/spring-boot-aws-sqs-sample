b14fae47de2de938ca5595286e5f7fa495b0eb64
hendisantika <hendisantika@yahoo.co.id>
2024-06-01 18:38:00 +0700

    Create Spring Boot 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootawssqssample/SpringBootAwsSqsSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootawssqssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsSqsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAwsSqsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootawssqssample/SpringBootAwsSqsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootawssqssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsSqsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
